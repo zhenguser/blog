fa7cbe4ef04bf496396dadbef942d2dc187bec26
zhenguser <zhengqiuser@163.com>
2020-03-01 01:31:39 +0800

    feat:添加了medium-zoom

--- config/pluginConf.js
+++ config/pluginConf.js
@@ -1,14 +1,21 @@
 module.exports = {
-    '@vuepress/pwa': {
-        serviceWorker: true,
-        updatePopup: {
-          message: "发现新内容可用。",
-          buttonText: "刷新"
-        }
-    },
-    '@vuepress/back-to-top': true,
-    '@vuepress/google-analytics':
-      {
-        'ga':'UA-00000000-0' // UA-00000000-0
-      }
+  '@vuepress/pwa': {
+    serviceWorker: true,
+    updatePopup: {
+      message: "发现新内容可用。",
+      buttonText: "刷新"
+    }
+  },
+  '@vuepress/back-to-top': true,
+  '@vuepress/google-analytics':{
+    'ga':'UA-00000000-0' // UA-00000000-0
+  },
+  '@vuepress/medium-zoom': {
+    selector: '.theme-default-content :not(a) > img',
+    // medium-zoom options here
+    // See: https://github.com/francoischalifour/medium-zoom#options
+    options: {
+      margin: 16
+    }
+  }
 };
--- utils/getFileName.js
+++ utils/getFileName.js
@@ -1,1 +1,23 @@
 //获取某个目录下的所有文件
+
+const { readdir, writeFile } = require('fs');
+const { resolve } = require('path');
+
+const FOLDERPATH = '';
+
+readdir(FOLDERPATH, (err, files) => {
+    let fileNames = [];
+    files.forEach(file => {
+        if( file === 'README.md') {
+            file = `''`;
+        }else {
+            file = file.replace('.md','');
+            file = `'${file}'`;
+        }
+        fileNames.push(file);
+    });
+
+    writeFile(resolve(__dirname, './filenames.js'), `[${fileNames}]`, ()=>{
+        console.log('文件获取完成');
+    })
+});
